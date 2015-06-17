d5690dd8319b6ba07a95f6740d2dd0f20858290a
diarmuid <contact@diarmuid.ie>
2015-06-17 21:03:32 +0100

    Remove unused dependencies

--- bootstrap/dependencies.php
+++ bootstrap/dependencies.php
@@ -9,12 +9,6 @@ use League\Flysystem\Adapter\Local as Adapter;
 
 use Intervention\Image\ImageManager;
 
-use Monolog\Logger;
-use Monolog\Handler\ErrorLogHandler;
-use Monolog\Handler\FingersCrossedHandler;
-
-use Monolog\ErrorHandler;
-
 use Diarmuidie\ImageRack\Http\Request;
 
 use Diarmuidie\ImageRack\Http\Response;
@@ -31,29 +25,9 @@ $container['cache'] = function ($c) {
     return new Filesystem(new Adapter(__DIR__.'/../storage/cache'));
 };
 
+// The Intervention image manager instance
 $container['imageManager'] = function ($c) {
-    // create an image manager instance with favored driver
     return new ImageManager();
-};
-
-// Whoops error page
-$container['errorPage'] = function ($c) {
-    $whoops = new Whoops\Run;
-    $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
-    return $whoops;
-};
-
-// Logger
-$container['logger'] = function ($c) {
-    $log = new Logger('ImageRack');
-    $errorHandler = new ErrorLogHandler(ErrorLogHandler::OPERATING_SYSTEM, Logger::DEBUG);
-    $log->pushHandler(new FingersCrossedHandler($errorHandler, Logger::DEBUG));
-    return $log;
-};
-
-// Error handler
-$container['errorHandler'] = function ($c) {
-    return new ErrorHandler($c['logger']);
 };
 
 // Request object
@@ -67,11 +41,5 @@ $container['response'] = function ($c) {
     $response = new Response();
     return $response;
 };
-
-// Register global error handler
-$container['errorHandler']->registerErrorHandler();
-
-// Register the error pages
-$container['errorPage']->register();
 
 return $container;
