7cb72d607edd4e5dbe91d706bc460425125deedc
diarmuid <contact@diarmuid.ie>
2015-06-17 21:27:47 +0100

    Refactor templates as callback

--- public/index.php
+++ public/index.php
@@ -4,12 +4,20 @@ error_reporting(E_ALL | E_STRICT);
 
 require 'vendor/autoload.php';
 
+// Load our dependencies
 $container = require_once __DIR__.'/../bootstrap/dependencies.php';
 
 // Run the imageRack server
 $server = new Diarmuidie\ImageRack\Server($container);
 
+// Array of templates
+$templates = array(
+    'small' => function () {
+        return new Templates\Small();
+    }
+);
+
 // Set the valid templates
-$server->setTemplates(['small']);
+$server->setTemplates($templates);
 
 $server->run();
